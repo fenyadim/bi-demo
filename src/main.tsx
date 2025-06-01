61910b66a6dd2a2c9b2134f09d2d12103c22c98a
fenyadim <dimos9813@gmail.com>
2025-06-01 22:03:42 +0800

    Fix fonts connecting

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import WebFont from 'webfontloader'
 import App from './App.tsx'
 import './globals.css'
 
-WebFont.load({
-	custom: {
-		families: ['BinancePlex: n2,n3,n4,n5'],
-		urls: ['/styles/fonts.css'],
-	},
-})
+// WebFont.load({
+// 	custom: {
+// 		families: ['BinancePlex: n2,n3,n4,n5'],
+// 		urls: ['/styles/fonts.css'],
+// 	},
+// })
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
 
 declare module '*.svg?react' {
