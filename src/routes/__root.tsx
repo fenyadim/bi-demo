35a7b1ca8f1152fdffe7c4b53a7016c10f297630
fenyadim <dimos9813@gmail.com>
2025-06-03 15:40:48 +0800

    fix: Исправил z-index в проекте

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,7 +9,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="theme">
-        <div className="fixed z-20 top-0 left-0 right-0 bg-background h-[70px]">
+        <div className="fixed z-60 top-0 left-0 right-0 bg-background h-[70px]">
           <PhoneHead />
         </div>
         <SheetProvider>
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
             <Outlet />
           </main>
         </SheetProvider>
-        <div className="fixed bottom-0 left-0 right-0 z-[60] flex items-center justify-center py-[9px]">
+        <div className="fixed bottom-0 left-0 right-0 z-70 flex items-center justify-center py-[9px]">
           <span className="block h-1 w-[158px] rounded-full bg-foreground" />
         </div>
       </ThemeProvider>
